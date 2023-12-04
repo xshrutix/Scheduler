f1567bd6c884b07672ea8445ad173de3e233b99f
xshrutix <shrutiisharma6@gmail.com>
2023-12-04 15:25:15 +0530

    msg

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navigation from './components/Navigation'
 import Home from './components/Home'
+import Guide from './components/Guide'
 
 const App = () => {
   return (
     <div>
       <Navigation />
      <Home />
+     <Guide />
     </div>
   )
 }
--- src/components/Guide.jsx
+++ src/components/Guide.jsx
@@ -1,9 +1,39 @@
 import React from 'react'
+import Navigation from './Navigation'
+import {MdInput} from 'react-icons/md'
+import {RiAiGenerate} from 'react-icons/ri'
+import {FaTableCells} from 'react-icons/fa6'
 
 const Guide = () => {
   return (
-    <div>
-        <h1 className=''>Guide Page</h1>
+    <div className=' w-full py[40rem] px-4 bg-gray-900'>
+      <h1 className=' text-white font-black text-4xl text-center'>Guide Page</h1>
+      <div className=' max-w-[1240px] mx-auto grid md:grid-cols-3 gap-8'>
+        <div className=' w-full shadow-xl flex flex-col p-4 my-56 rounded-lg hover:scale-105 duration-300 bg-white'>
+           <MdInput
+           className=' w-[80px] h-[80px] mx-auto pb-12 ' 
+           />
+           <h2 className=' text-2xl font-bold text-center py-4'>Step 1</h2>
+           <p className=' text-center'>Input your data</p>
+        </div>
+
+        <div className=' w-full shadow-xl flex flex-col p-4 my-56 rounded-lg hover:scale-105 duration-300 bg-white'>
+           <RiAiGenerate
+           className=' w-[80px] h-[80px] mx-auto pb-12 ' 
+           />
+           <h2 className=' text-2xl font-bold text-center py-4'>Step 2</h2>
+           <p className=' text-center'>Generate Table</p>
+        </div>
+
+        <div className=' w-full shadow-xl flex flex-col p-4 my-56 rounded-lg hover:scale-105 duration-300 bg-white'>
+           <FaTableCells
+           className=' w-[80px] h-[80px] mx-auto pb-12 ' 
+           />
+           <h2 className=' text-2xl font-bold text-center py-4'>Step 3</h2>
+           <p className=' text-center'>Select View</p>
+        </div>
+
+      </div>
 
     </div>
   )
